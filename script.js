05c76e2562efd2305c67db294ad31c8a209c7813
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-14 19:01:15 -0400

    Code playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
     let choice = Math.random();
@@ -14,4 +17,31 @@ function getHumanChoice() {
     return prompt(`Rock, paper, or scissors?`).toLowerCase();
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Same choice, it's a draw!`);
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore++;
+        console.log('Paper beats rock. Computer wins!');
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        computerScore++;
+        console.log(`Scissors beats paper. Computer wins.`);
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore++;
+        console.log('Rock smashes scissors into tiny smitherines. Computer is victorious.');
+
+    } else if (computerChoice === 'rock' && humanChoice === 'paper') {
+        humanScore++;
+        console.log('Paper suffocates rock. Meatbag wins!');
+    } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
+        humanScore++;
+        console.log(`Scissors slices paper. You win this time, human.`);
+    } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
+        humanScore++;
+        console.log('Rock smashes scissors into tiny smitherines. You are victorious.');
+    }
+}
+
+
+
+playRound(getHumanChoice(), getComputerChoice());
