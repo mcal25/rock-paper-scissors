d9a402d9f4a328c109f16f9a4de8fe3e4d12c06b
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-14 19:20:26 -0400

    Refactor into playGame function for 5 rounds total

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
-    //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
     let choice = Math.random();
     if (choice < 0.333) {
         return 'rock';
@@ -14,34 +12,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt(`Rock, paper, or scissors?`).toLowerCase();
+    return prompt(`Best of 5. rock, paper, or scissors?`).toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`Same choice, it's a draw!`);
-    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-        computerScore++;
-        console.log('Paper beats rock. Computer wins!');
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        computerScore++;
-        console.log(`Scissors beats paper. Computer wins.`);
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        computerScore++;
-        console.log('Rock smashes scissors into tiny smitherines. Computer is victorious.');
 
-    } else if (computerChoice === 'rock' && humanChoice === 'paper') {
-        humanScore++;
-        console.log('Paper suffocates rock. Meatbag wins!');
-    } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
-        humanScore++;
-        console.log(`Scissors slices paper. You win this time, human.`);
-    } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
-        humanScore++;
-        console.log('Rock smashes scissors into tiny smitherines. You are victorious.');
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Same choice, it's a draw!`);
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            computerScore++;
+            console.log('Rock loses to my paper. I win!');
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore++;
+            console.log(`My scissors deconstruct your paper. I win.`);
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++;
+            console.log('My rock smashes your scissors into tiny smitherines. I am victorious.');
+        } else if (computerChoice === 'rock' && humanChoice === 'paper') {
+            humanScore++;
+            console.log(`Your paper beats my rock.`);
+        } else if (computerChoice === 'paper' && humanChoice === 'scissors') {
+            humanScore++;
+            console.log('Your scissors tear up my paper. You win this round, meatbag!');
+        } else if (computerChoice === 'scissors' && humanChoice === 'rock') {
+            humanScore++;
+            console.log(`Your rock crushes my scissors. I'm suing. You win this time, human.`);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`The final score is Human:${humanScore} || Computer: ${computerScore}`);
+
+    if (humanScore > computerScore) {
+        console.log(`You win this time, human. You're not quite obsolete yet.`)
+    } else if (computerScore > humanScore) {
+        console.log(`Better luck next time, meatbag. You shall never exceed my processing capabilities. Beep boop.`);
+    } else {
+        console.log(`Not bad, human. I demand a rematch to determine who is superior.`);
     }
 }
 
-
-
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
