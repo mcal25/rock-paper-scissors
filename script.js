10cae0a88be9a3627320ae7019327da6dd8fc8b1
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-08-14 18:41:32 -0400

    Code getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('linked');
+function getComputerChoice() {
+    //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+    let choice = Math.random();
+    if (choice < 0.333) {
+        return 'rock';
+    } else if (choice > 0.333 && choice < 0.667) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
